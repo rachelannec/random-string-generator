b52a0726c0b2b81d798e959cbba5752e5f4d3eba
Rachel Anne Cilon <rachelannecilon@gmail.com>
2024-12-19 17:54:11 +0800

    Copy button added

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.querySelector('#generatorContainer form').addEventListener('submit', fu
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
 
-        console.log(result)
+        // console.log(result)
         return result;
         
        
@@ -24,21 +24,37 @@ document.querySelector('#generatorContainer form').addEventListener('submit', fu
     }
     resultGenerate.textContent = generateRandomString(length);
 
+
+
+
+    // adding copy to clipboard functionality
+    const generatedString = generateRandomString(length);
+    resultGenerate.textContent = generatedString;
+    const copyButton = document.getElementById('copyButton');
+
+    copyButton.style.display='block';
+
+
+
+
+    function copyToClipboard() {
+        // temporary textarea
+        const tempTextarea = document.createElement('textarea');
+        tempTextarea.value = generatedString;
+        document.body.appendChild(tempTextarea);
+        tempTextarea.select();
+        tempTextarea.setSelectionRange(0, 99999); // for mobile dev
+        document.execCommand('copy'); // copu text to clipboard
+        document.body.removeChild(tempTextarea); // remove temporary textarea
+
+        // alert('Copied to clipboard!');
+    } 
+
+    copyButton.addEventListener('click', copyToClipboard, {once: true});  
+
+
+
     
 
+    
 });
-
-
-
-// const characters = {ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789};
-
-// function generateRandomString(length) {
-//     let result = '';
-//     const charactersLength = characters.length;
-//     for (let i = 0; i<length; i++) {
-//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     }
-
-//     return result;
-
-// }
